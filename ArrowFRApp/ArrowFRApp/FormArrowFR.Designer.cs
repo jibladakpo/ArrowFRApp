f79a1f062f4ffbcf90cda8104a416d9516cbf840
jibladakpo <ladakpo.jibril@gmail.com>
2018-02-14 11:58:41 +0100

    connexion utilisateur
    connecter un utilisateur
    problème message box s'affiche même si la saisie est bonne

--- ArrowFRApp/ArrowFRApp/FormArrowFR.Designer.cs
+++ ArrowFRApp/ArrowFRApp/FormArrowFR.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Accueil));
-            this.buttonEntrer = new System.Windows.Forms.Button();
+            this.buttonConnexion = new System.Windows.Forms.Button();
             this.pictureArrowFR = new System.Windows.Forms.PictureBox();
             this.labelTitre = new System.Windows.Forms.Label();
             this.panelAccueil = new System.Windows.Forms.Panel();
@@ -40,23 +40,23 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureArrowFR)).BeginInit();
             this.SuspendLayout();
             // 
-            // buttonEntrer
+            // buttonConnexion
             // 
-            this.buttonEntrer.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.buttonEntrer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonEntrer.Location = new System.Drawing.Point(649, 574);
-            this.buttonEntrer.Name = "buttonEntrer";
-            this.buttonEntrer.Size = new System.Drawing.Size(275, 82);
-            this.buttonEntrer.TabIndex = 0;
-            this.buttonEntrer.Text = "Se Connecter";
-            this.buttonEntrer.UseVisualStyleBackColor = true;
-            this.buttonEntrer.Click += new System.EventHandler(this.button1_Click);
+            this.buttonConnexion.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonConnexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConnexion.Location = new System.Drawing.Point(649, 574);
+            this.buttonConnexion.Name = "buttonConnexion";
+            this.buttonConnexion.Size = new System.Drawing.Size(275, 82);
+            this.buttonConnexion.TabIndex = 0;
+            this.buttonConnexion.Text = "Se Connecter";
+            this.buttonConnexion.UseVisualStyleBackColor = true;
+            this.buttonConnexion.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureArrowFR
             // 
             this.pictureArrowFR.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.pictureArrowFR.Image = ((System.Drawing.Image)(resources.GetObject("pictureArrowFR.Image")));
-            this.pictureArrowFR.Location = new System.Drawing.Point(649, 100);
+            this.pictureArrowFR.Location = new System.Drawing.Point(649, 101);
             this.pictureArrowFR.Name = "pictureArrowFR";
             this.pictureArrowFR.Size = new System.Drawing.Size(275, 272);
             this.pictureArrowFR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -73,16 +73,15 @@
             this.labelTitre.Size = new System.Drawing.Size(606, 69);
             this.labelTitre.TabIndex = 2;
             this.labelTitre.Text = "Association ArrowFR";
-            this.labelTitre.Click += new System.EventHandler(this.label1_Click);
             // 
             // panelAccueil
             // 
             this.panelAccueil.BackColor = System.Drawing.SystemColors.Window;
-            this.panelAccueil.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelAccueil.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelAccueil.Location = new System.Drawing.Point(0, 0);
             this.panelAccueil.Name = "panelAccueil";
-            this.panelAccueil.Size = new System.Drawing.Size(114, 689);
-            this.panelAccueil.TabIndex = 3;
+            this.panelAccueil.Size = new System.Drawing.Size(1580, 689);
+            this.panelAccueil.TabIndex = 5;
             this.panelAccueil.Visible = false;
             // 
             // labelLogin
@@ -90,10 +89,10 @@
             this.labelLogin.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.labelLogin.AutoSize = true;
             this.labelLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelLogin.Location = new System.Drawing.Point(646, 400);
+            this.labelLogin.Location = new System.Drawing.Point(647, 390);
             this.labelLogin.Name = "labelLogin";
             this.labelLogin.Size = new System.Drawing.Size(91, 32);
-            this.labelLogin.TabIndex = 4;
+            this.labelLogin.TabIndex = 2;
             this.labelLogin.Text = "Login";
             // 
             // labelMdp
@@ -104,7 +103,7 @@
             this.labelMdp.Location = new System.Drawing.Point(647, 476);
             this.labelMdp.Name = "labelMdp";
             this.labelMdp.Size = new System.Drawing.Size(196, 32);
-            this.labelMdp.TabIndex = 5;
+            this.labelMdp.TabIndex = 0;
             this.labelMdp.Text = "Mot de passe";
             // 
             // textLogin
@@ -114,7 +113,7 @@
             this.textLogin.Location = new System.Drawing.Point(649, 435);
             this.textLogin.Name = "textLogin";
             this.textLogin.Size = new System.Drawing.Size(275, 38);
-            this.textLogin.TabIndex = 6;
+            this.textLogin.TabIndex = 0;
             // 
             // textMdp
             // 
@@ -122,8 +121,9 @@
             this.textMdp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textMdp.Location = new System.Drawing.Point(649, 508);
             this.textMdp.Name = "textMdp";
+            this.textMdp.PasswordChar = '*';
             this.textMdp.Size = new System.Drawing.Size(275, 38);
-            this.textMdp.TabIndex = 7;
+            this.textMdp.TabIndex = 0;
             // 
             // Accueil
             // 
@@ -131,14 +131,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RoyalBlue;
             this.ClientSize = new System.Drawing.Size(1580, 689);
-            this.Controls.Add(this.textMdp);
-            this.Controls.Add(this.textLogin);
-            this.Controls.Add(this.labelMdp);
-            this.Controls.Add(this.labelLogin);
             this.Controls.Add(this.panelAccueil);
             this.Controls.Add(this.labelTitre);
             this.Controls.Add(this.pictureArrowFR);
-            this.Controls.Add(this.buttonEntrer);
+            this.Controls.Add(this.buttonConnexion);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.labelMdp);
+            this.Controls.Add(this.textMdp);
+            this.Controls.Add(this.textLogin);
             this.Name = "Accueil";
             this.Text = "ArrowFRApp";
             ((System.ComponentModel.ISupportInitialize)(this.pictureArrowFR)).EndInit();
@@ -149,7 +149,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button buttonEntrer;
+        private System.Windows.Forms.Button buttonConnexion;
         private System.Windows.Forms.PictureBox pictureArrowFR;
         private System.Windows.Forms.Label labelTitre;
         private System.Windows.Forms.Panel panelAccueil;
--- ArrowFRApp/ArrowFRApp/FormArrowFR.cs
+++ ArrowFRApp/ArrowFRApp/FormArrowFR.cs
@@ -1,6 +1,16 @@
 ﻿using ArrowFRApp.Controls;
 using System;
+using System.Collections.Generic;
+using ArrowFRApp.Classes;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using ArrowFRApp.DataAccessLayer;
+using MySql.Data.MySqlClient;
 
 namespace ArrowFRApp
 {
@@ -15,31 +25,35 @@ namespace ArrowFRApp
         private void button1_Click(object sender, EventArgs e)
         {
             panelAccueil.Visible = true;
+            string connectionString = Initialisation.InitialiserConnexion();
+            MySqlConnection sqlcon = new MySqlConnection(connectionString);
+            string query = "Select * From Utilisateur Where login = '" + textLogin.Text.Trim() + "' and mdp = '" + textMdp.Text.Trim() + "'"; 
+            MySqlDataAdapter sda =  new MySqlDataAdapter(query, sqlcon);
+            DataTable dt1 = new DataTable();
+            sda.Fill(dt1);
 
-             if (!panelAccueil.Controls.Contains(GestionAccueil.Instance))
+
+            if (dt1.Rows.Count == 1)
             {
                 panelAccueil.Controls.Add(GestionAccueil.Instance);
                 GestionAccueil.Instance.Dock = DockStyle.Fill;
                 GestionAccueil.Instance.BringToFront();
             }
             else
-                GestionAccueil.Instance.BringToFront();
-        
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
+                panelAccueil.Visible = false;
+            MessageBox.Show("Vérifier votre login et mot de passe");
+            
 
         }
 
-       /* private void panelAccueil_Paint(object sender, PaintEventArgs e)
-        {
+        /* private void panelAccueil_Paint(object sender, PaintEventArgs e)
+         {
 
-        } */
+         } */
 
-       /* private void panelGestion_Paint(object sender, PaintEventArgs e)
-        {
+        /* private void panelGestion_Paint(object sender, PaintEventArgs e)
+         {
 
-        } */
+         } */
     }
 }
